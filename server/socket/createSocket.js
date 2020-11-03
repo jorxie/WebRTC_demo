d01bc76627945fd5c177d34129b10031a90948c6
Joe.Tse <709536505@qq.com>
2020-11-03 10:54:32 +0800

    修复房间聊天问题

--- server/socket/createSocket.js
+++ server/socket/createSocket.js
@@ -5,21 +5,30 @@ const createSocket = function(server){
   });
   io.on('connection', clientSocket => {
     let roomNumberMap = {}; //房间人数
+    let curRoomName = '';
     //加入房间
     clientSocket.on("joinroom",function (args) {
       const data = JSON.parse(args||"{}")
-      if(roomNumberMap[data.roomName]>=2) return; //一个房间只有能有两个人
-      clientSocket.join(data.roomName); // join(房间名)加入房间
-      io.to(data.roomName).emit('user-joined',data.userId);
-      roomNumberMap[data.roomName]? ++roomNumberMap[data.roomName] : roomNumberMap[data.roomName] = 1;
+      //暂时做成以最后一个房间号为准
+      curRoomName = data.roomName;
+      if(roomNumberMap[curRoomName]>=2){
+        //一个房间只有能有两个人
+        console.log('one room 2 persons at most')
+        return; 
+      } 
+      //join(房间名)加入房间
+      clientSocket.join(curRoomName); 
+      io.to(curRoomName).emit('user-joined',data.userId);
+      roomNumberMap[curRoomName]? ++roomNumberMap[curRoomName] : roomNumberMap[curRoomName] = 1;
     });
     //退出 离开房间
     clientSocket.on("leaveRoom",function (roomName) {
-        clientSocket.leave(roomName);//leave(房间名) 离开房间
+        //leave(房间名) 离开房间
+        clientSocket.leave(roomName);
     });
     clientSocket.on('broadcast', params => {
       //发送给该房间其他人（除了发送者）
-      clientSocket.io(data.roomName).emit('broadcast',params)
+      clientSocket.to(curRoomName).emit('broadcast',params)
     });
     clientSocket.on('disconnect', () => { /* … */ });
   });
